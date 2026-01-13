853f25419cbab9b3cb574dc951242237e987fb52
Avi Singh <aviop2005@gmail.com>
2026-01-13 21:03:10 +0000

    Add initial unit tests for calculator application using GitHub Copilot

--- test/arithmetic.test.js
+++ test/arithmetic.test.js
@@ -94,7 +94,49 @@ describe('Arithmetic', function () {
     });
 
 // TODO: Challenge #1
- 
+ // Test case for power function
+    describe('Power', function () {
+        it('raises a positive integer to a positive integer power', function (done) {
+            request.get('/arithmetic?operation=power&operand1=2&operand2=3')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: 8 });
+                    done();
+                });
+        });
+        it('raises a positive integer to the zero power', function (done) {
+            request.get('/arithmetic?operation=power&operand1=5&operand2=0')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: 1 });
+                    done();
+                });
+        });
+        it('raises a negative integer to an even integer power', function (done) {
+            request.get('/arithmetic?operation=power&operand1=-2&operand2=4')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: 16 });
+                    done();
+                });
+        });
+        it('raises a negative integer to an odd integer power', function (done) {
+            request.get('/arithmetic?operation=power&operand1=-2&operand2=3')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: -8 });
+                    done();
+                });
+        });
+        it('raises a floating point number to a positive integer power', function (done) {
+            request.get('/arithmetic?operation=power&operand1=1.5&operand2=2')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: 2.25 });
+                    done();
+                });
+        });
+    });
 
     describe('Multiplication', function () {
         it('multiplies two positive integers', function (done) {
